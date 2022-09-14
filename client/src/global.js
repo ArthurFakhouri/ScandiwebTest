6d08eacde94f026eab75607b64cd22f1b8085981
ArthurFakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2022-09-14 01:33:57 -0300

    refactor: changing global.css file to styled-component file

---
+++ client/src/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  html, body{
+    height: 100%;
+  }
+
+  body {
+    background-color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: Raleway, sans-serif;
+  }
+`
