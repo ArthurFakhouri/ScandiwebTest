0a2737f896c00ef281b8ad68f3e1c3318ac72523
ArthurFakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2022-09-14 01:47:12 -0300

    feat: adding style to App.js file

---
+++ client/src/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+    width: 100%;
+    height: 100%;
+    user-select: none;
+    padding-top: 80px;
+    ${props=>props.isLookingCart?
+    `background-color: rgba(57, 55, 72, 0.22);
+    filter: brightness(.82);
+    pointer-events: none;
+    `:""}
+    main {
+        padding: 0 7%;
+        padding-top: 60px;
+    }
+`
