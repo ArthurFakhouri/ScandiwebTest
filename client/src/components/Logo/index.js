04f71b8b5cf2e60acab67995a4060699c2b3cfad
ArthurFakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2022-10-19 10:42:24 -0300

    refactor: allowing to redirect to PLP when Logo is clicked

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { PureComponent } from 'react';
+import { withRouter } from 'react-router-dom';
 import BrandIcon from '../../assets/brandIcon.svg';
 import { LogoContainer } from './style';
 
 class Logo extends PureComponent {
+
     render() {
+        const { history } = this.props;
+
         return (
             <LogoContainer>
-                <img src={BrandIcon} alt="Brand Icon" />
+                <img onClick={()=>history.push("/")} src={BrandIcon} alt="Brand Icon" />
             </LogoContainer>
         );
     }
 }
 
-export default Logo;
+export default withRouter(Logo);
